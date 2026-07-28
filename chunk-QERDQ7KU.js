ea0fbb4dbff9d73d5ebe8246e5446ddc6d26132b
YousufFFFF <141269047+YousufFFFF@users.noreply.github.com>
2026-07-28 19:21:16 +0000

    Deploying to gh-pages from @ YousufFFFF/web-app@dcf5148160ac84e9c9512ed0ac5866b3b714a9e0 🚀

---
+++ chunk-QERDQ7KU.js
@@ -0,0 +1,1 @@
+import{Ka as r}from"./chunk-E2A6G2WC.js";import{ea as e,ja as c}from"./chunk-WZKB436B.js";var u=(()=>{class t{constructor(){this.accountingService=c(r)}resolve(){return this.accountingService.getOffices()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac})}}return t})();export{u as a};

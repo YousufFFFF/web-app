d482669980284da488adc557e003f6b1a1cb6b4a
YousufFFFF <141269047+YousufFFFF@users.noreply.github.com>
2026-07-31 06:40:04 +0000

    Deploying to gh-pages from @ YousufFFFF/web-app@5d15ec12d319be26422450395cb1f303560ea789 🚀

--- chunk-U4J73KCY.js
+++
@@ -1,1 +0,0 @@
-import{n as i}from"./chunk-FTVHA3US.js";import{ea as e,ja as r}from"./chunk-3K7SZNBL.js";var m=(()=>{class t{constructor(){this.systemService=r(i)}resolve(){return this.systemService.getConfigurations()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{m as a};

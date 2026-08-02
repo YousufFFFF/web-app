ae45670115dcb3a09d25dcd7d2a2ad688bd128bc
YousufFFFF <141269047+YousufFFFF@users.noreply.github.com>
2026-08-02 21:15:02 +0000

    Deploying to gh-pages from @ YousufFFFF/web-app@395890cf140bd9acf2b0d7b0bccedb20e491ccc4 🚀

---
+++ chunk-763ZNAWA.js
@@ -0,0 +1,1 @@
+import{a as i}from"./chunk-ECO7YN62.js";import{ea as r,ja as e}from"./chunk-ZB5RS2MB.js";var m=(()=>{class t{constructor(){this.organizationService=e(i)}resolve(){return this.organizationService.getOffices()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac})}}return t})();export{m as a};

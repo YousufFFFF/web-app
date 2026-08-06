990526df1c741bda0b207b2da3007cfdd96e9ffb
YousufFFFF <141269047+YousufFFFF@users.noreply.github.com>
2026-08-06 13:40:46 +0000

    Deploying to gh-pages from @ YousufFFFF/web-app@3772e1b32692ff4f9ef951db9eb23be8d85a8792 🚀

--- chunk-7IGDFJ4M.js
+++
@@ -1,1 +0,0 @@
-import{n as i}from"./chunk-E773423F.js";import{ea as e,ja as r}from"./chunk-6ML7VZTO.js";var m=(()=>{class t{constructor(){this.systemService=r(i)}resolve(){return this.systemService.getConfigurations()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{m as a};

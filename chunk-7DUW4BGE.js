b78a436058d2b72af376c776ba8bf19c04d9c8bd
YousufFFFF <141269047+YousufFFFF@users.noreply.github.com>
2026-08-18 23:16:37 +0000

    Deploying to gh-pages from @ YousufFFFF/web-app@1aa4e3eab2fe4b273faee99044190c9393a12f82 🚀

--- chunk-7DUW4BGE.js
+++
@@ -1,1 +0,0 @@
-import{Qa as r}from"./chunk-6ROKRICN.js";import{W as e,_ as c}from"./chunk-PYBCK6S7.js";var u=(()=>{class t{constructor(){this.accountingService=c(r)}resolve(){return this.accountingService.getOffices()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac})}}return t})();export{u as a};
--- chunk-PVJJGAAB.js
+++
@@ -1,1 +0,0 @@
-import{n as i}from"./chunk-HQDUD5YU.js";import{W as e,_ as r}from"./chunk-PYBCK6S7.js";var m=(()=>{class t{constructor(){this.systemService=r(i)}resolve(){return this.systemService.getConfigurations()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{m as a};

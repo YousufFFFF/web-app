8cfc43de0f6519293f7f917476b5a497df996aec
YousufFFFF <141269047+YousufFFFF@users.noreply.github.com>
2026-08-31 17:19:41 +0000

    Deploying to gh-pages from @ YousufFFFF/web-app@774c69183db5e39e9db06694693ae9e00f123837 🚀

--- chunk-GJCXYCPV.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./chunk-DHP7T5IH.js";import{V as r,Z as e}from"./chunk-FOVXYDXU.js";var m=(()=>{class t{constructor(){this.organizationService=e(i)}resolve(){return this.organizationService.getOffices()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac})}}return t})();export{m as a};
